843e9fb669808bc7fb848cefddca4c827e9bf9a9
Vidath Harshitha <68719470+Harshavidath12@users.noreply.github.com>
2026-06-26 19:44:59 +0530

    Add login authentication logic

---
+++ Frontend/src/login.ts
@@ -0,0 +1,47 @@
+// @ts-nocheck
+import './style.css'
+import Alpine from 'alpinejs'
+import axios from 'axios'
+import { setToken, removeToken, setAxiosAuth } from './auth'
+
+window.Alpine = Alpine
+
+axios.defaults.baseURL = 'http://localhost:8000/api'
+axios.defaults.headers.common['Accept'] = 'application/json'
+
+// Redirect already-logged-in users
+if (localStorage.getItem('auth_token')) {
+  window.location.href = '/'
+}
+
+document.addEventListener('alpine:init', () => {
+  Alpine.data('loginPage', () => ({
+    form: { email: '', password: '' },
+    loading: false,
+    errorMsg: '',
+
+    async login() {
+      this.loading = true
+      this.errorMsg = ''
+      try {
+        const res = await axios.post('/auth/login', this.form)
+        setToken(res.data.token)
+        localStorage.setItem('auth_user', JSON.stringify(res.data.user))
+        window.location.href = '/'
+      } catch (err) {
+        if (err.response?.data?.errors) {
+          const msgs = Object.values(err.response.data.errors).flat()
+          this.errorMsg = msgs[0]
+        } else if (err.response?.data?.message) {
+          this.errorMsg = err.response.data.message
+        } else {
+          this.errorMsg = 'Login failed. Please try again.'
+        }
+      } finally {
+        this.loading = false
+      }
+    }
+  }))
+})
+
+Alpine.start()
